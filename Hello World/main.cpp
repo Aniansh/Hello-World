15e9b6eb27f4ec2fee415a05c0b7481b07c38e1e
Aniansh Raj Singh <aniansh@yahoo.com>
2015-11-27 17:10:25 +0530

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Aniansh Raj on 27/11/15.
+//  Copyright © 2015 Aniansh Raj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
