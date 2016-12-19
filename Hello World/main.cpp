68f7f3d42ff37e5de0a3fe9e456805cb69bb0e15
Aniansh <aniansh@yahoo.com>
2016-12-19 17:08:13 +0530

    nothing muvh

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -1,15 +1,15 @@
-//
-//  main.cpp
-//  Hello World
-//
-//  Created by Aniansh Raj on 27/11/15.
-//  Copyright © 2015 Aniansh Raj. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <strings.h>
+using namespace std;
+int main()
+{
+  
+    cout <<"Type your name\n";
+    string ariel;
+    
+    cin >> ariel;
+    cout <<"hey " << ariel << " i am alive and also HELLO WORLD! \n";
+    cout<<"hey\n";
+    
+    
 }
