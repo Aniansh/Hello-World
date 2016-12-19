43fdec2cce925cc0d4be5686ea4bab7ab846dfe3
Aniansh <aniansh@yahoo.com>
2016-12-19 19:26:29 +0530

    third commit

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -10,7 +10,8 @@ int main()
     cin >> ariel;
     cout <<"hey " << ariel << " i am alive and also HELLO WORLD! \n";
     cout<<"hey\n";
-    cout<<"babay";
+    cout<<"babayaa";;
+    //i changed something
     
     
 }
